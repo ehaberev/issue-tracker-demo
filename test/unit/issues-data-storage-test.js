64ee759d3b5648c994d3c518a5f8a627011a26ae
Evgeniy Khaberev <ehaberev@yandex.ru>
2019-11-11 03:21:35 +0500

    chore: fix `Add issue` test

--- test/unit/issues-data-storage-test.js
+++ test/unit/issues-data-storage-test.js
@@ -1,7 +1,7 @@
 /* global QUnit */
 import IssuesDataStorage from '../../src/issues-data-storage';
 
-QUnit.module('IssuesDataStorage tests', {
+QUnit.module('Unit | IssuesDataStorage', {
   beforeEach: () => {
     // prepare something before each test
   },
@@ -15,7 +15,7 @@ QUnit.test('Add issue', assert => {
   const tempDataService = {
     items: [],
     loadEntities() {
-      return this.items;
+      return JSON.parse(JSON.stringify(this.items));
     },
     addEntity(entity) {
       this.items.push(entity);
@@ -26,16 +26,20 @@ QUnit.test('Add issue', assert => {
   tempDataService.items = [];
   const issuesDataStorage = new IssuesDataStorage(tempDataService);
 
-  // Act
-  const result = issuesDataStorage.createIssue({
+  const issueToAdd = {
     id: '234-13425-21223',
     description: 'Сломался утюг',
     severity: 'Low',
     assignedTo: 'Иванов И.И',
     status: 'Open',
-  });
+  };
+
+  // Act
+  const result = issuesDataStorage.createIssue(issueToAdd);
+  const addedIssue = issuesDataStorage.issues.find(issue => issue.id === issueToAdd.id);
 
   // Assert
   assert.equal(result, true, 'Issue has been added');
   assert.equal(issuesDataStorage.issues.length, 1, 'Count of issues is correct');
+  assert.notEqual(addedIssue, undefined, 'Issue was found in storage');
 });
